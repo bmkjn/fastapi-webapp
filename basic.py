ef715a91434688e08d19d0131eacc87821c798e2
Bhumika Jain <bhumika.jain@fractal.ai>
2025-10-13 20:12:19 +0530

    initial

---
+++ basic.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/greet")
+def greet(name: str = "Guest"):
+    return {"message": f"Hello, {name}!"}
