8a275b313f3d63bdd4db59e78051dcaed9d2caf0
bmkjn <bhumiika.jain@gmail.com>
2025-10-16 15:47:11 +0530

    edit connection string

--- app_insights_logger.py
+++ app_insights_logger.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.INFO)
 # Attach Azure handler
 logger.addHandler(
     AzureLogHandler(
-        connection_string="InstrumentationKey=19c9c53e-448a-4953-9374-a782b6392735"
+        connection_string="InstrumentationKey=19c9c53e-448a-4953-9374-a782b6392735;IngestionEndpoint=https://uaenorth-0.in.applicationinsights.azure.com/;LiveEndpoint=https://uaenorth.livediagnostics.monitor.azure.com/;ApplicationId=a70e5645-1ce5-49bc-b5c1-6e25d57d8a23"
     )
 )
 
